15e734c0ad4fded85dfae5190c72febd4973429e
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-10-29 21:58:43 +0900

    add DuckDB::Database#connection method

--- ext/duckdb/connection.h
+++ ext/duckdb/connection.h
@@ -8,5 +8,6 @@ struct _rubyDuckDBConnection {
 typedef struct _rubyDuckDBConnection rubyDuckDBConnection;
 
 void init_duckdb_connection(void);
+VALUE create_connection(VALUE oDuckDBDatabase);
 
 #endif
