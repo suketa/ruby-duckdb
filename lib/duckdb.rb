d1a61f92ee644ae1da56708fa6b254c7f0a372d7
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-11-03 15:36:57 +0900

    DuckDB::Result include Enumerable

--- lib/duckdb.rb
+++ lib/duckdb.rb
@@ -1,5 +1,6 @@
-require "duckdb/version"
-require "duckdb/duckdb_native"
+require 'duckdb/version'
+require 'duckdb/duckdb_native'
+require 'duckdb/result'
 
 module DuckDB
 end
---
+++ lib/duckdb/result.rb
@@ -0,0 +1,5 @@
+module DuckDB
+  class Result
+    include Enumerable
+  end
+end
