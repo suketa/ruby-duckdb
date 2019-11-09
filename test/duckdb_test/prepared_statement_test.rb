fac9fa86af19ede5096814b5c255e650de41c1d0
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-11-09 15:01:23 +0900

    add DuckDB::PreparedStatement#nparams
    support prepared statement step 4.

--- test/duckdb_test/prepared_statement_test.rb
+++ test/duckdb_test/prepared_statement_test.rb
@@ -23,6 +23,7 @@ module DuckDBTest
       assert_raises(ArgumentError) { DuckDB::PreparedStatement.new }
       assert_raises(TypeError) { DuckDB::PreparedStatement.new(con, 1) }
       assert_raises(TypeError) { DuckDB::PreparedStatement.new(1, 1) }
+      assert_raises(DuckDB::Error) { DuckDB::PreparedStatement.new(con, 'SELECT * FROM') }
     end
 
     def test_execute
@@ -30,6 +31,18 @@ module DuckDBTest
       stmt = DuckDB::PreparedStatement.new(con, 'SELECT * FROM a')
       result = stmt.execute
       assert_instance_of(DuckDB::Result, result)
+    end
+
+    def test_nparams
+      con = PreparedStatementTest.con
+      stmt = DuckDB::PreparedStatement.new(con, 'SELECT * FROM a')
+      assert_equal(0, stmt.nparams)
+
+      stmt = DuckDB::PreparedStatement.new(con, 'SELECT * FROM a WHERE id = $1')
+      assert_equal(1, stmt.nparams)
+
+      stmt = DuckDB::PreparedStatement.new(con, 'SELECT * FROM a WHERE id = ?')
+      assert_equal(1, stmt.nparams)
     end
   end
 end
