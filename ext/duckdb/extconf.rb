f99c7018f59cfa4d21bdbfd52ecef0c848b05acb
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-11-14 20:41:33 +0900

    add DuckDB::PreparedStatement#bind_null

--- ext/duckdb/extconf.rb
+++ ext/duckdb/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 
 dir_config('duckdb')
-have_library('duckdb')
-have_func('duckdb_bind_null', 'duckdb.h')
-create_makefile('duckdb/duckdb_native')
+if have_library('duckdb')
+  have_func('duckdb_bind_null', 'duckdb.h')
+  create_makefile('duckdb/duckdb_native')
+end
