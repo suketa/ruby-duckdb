be9764a95df8cd2aec89dbbf75f60c8533d3c619
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2020-02-02 07:26:59 +0900

    add more Connection#query test

--- test/duckdb_test/connection_test.rb
+++ test/duckdb_test/connection_test.rb
@@ -10,7 +10,15 @@ module DuckDBTest
       assert_instance_of(DuckDB::Result, @con.query('CREATE TABLE table1 (id INTEGER)'))
     end
 
-    def test_query_argument
+    def test_query_with_valid_params
+      @con.query('CREATE TABLE t (col1 INTEGER, col2 STRING)')
+      assert_instance_of(DuckDB::Result, @con.query('INSERT INTO t VALUES(?, ?)', 1, 'a'))
+      r = @con.query('SELECT col1, col2 FROM t WHERE col1 = ? and col2 = ?', 1, 'a')
+      assert_equal(1, r.each.first[0])
+      assert_equal('a', r.each.first[1])
+    end
+
+    def test_query_with_invalid_params
       assert_raises(DuckDB::Error) { @con.query('foo', 'bar') }
 
       assert_raises(ArgumentError) { @con.query }
