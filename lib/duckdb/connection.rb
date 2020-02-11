e45640b776e729808944857d1f1bae0b29b32bd8
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2020-02-11 15:16:14 +0900

    add DuckDB::Connection documentation

--- lib/duckdb/connection.rb
+++ lib/duckdb/connection.rb
@@ -1,5 +1,24 @@
 module DuckDB
+  # The DuckDB::Connection encapsulates connection with DuckDB database.
+  #
+  #   require 'duckdb'
+  #   db = DuckDB::Database.open
+  #   con = db.connect
+  #   con.query(sql)
   class Connection
+    #
+    # executes sql with args.
+    # The first argument sql must be SQL string.
+    # The rest arguments are parameters of SQL string.
+    # The parameters must be '?' in SQL statement.
+    #
+    #   require 'duckdb'
+    #   db = DuckDB::Database.open('duckdb_file')
+    #   con = db.connect
+    #   users = con.query('SELECT * FROM users')
+    #   sql = 'SELECT * FROM users WHERE name = ? AND email = ?'
+    #   dave = con.query(sql, 'Dave', 'dave@example.com')
+    #
     def query(sql, *args)
       return query_sql(sql) if args.empty?
 
