a9e8d19deedaf2a8d4b363bf77a70bd7a5e9b07a
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-04-30 08:08:20 +0900

    make duckdb version check more usefull

--- test/duckdb_test/duckdb_version.rb
+++ test/duckdb_test/duckdb_version.rb
@@ -1,16 +1,28 @@
 module DuckDBTest
-  module DuckDBVersion
+  class DuckDBVersion
+    include Comparable
+
     def self.duckdb_version
       return @duckdb_version if @duckdb_version
 
       DuckDB::Database.open('version.duckdb') do |db|
         db.connect do |con|
           r = con.query('SELECT VERSION();')
-          @duckdb_version = Gem::Version.new(r.first.first.sub('v', ''))
+          @duckdb_version = DuckDBVersion.new(r.first.first.sub('v', ''))
         end
       end
     ensure
       FileUtils.rm_f('version.duckdb')
+    end
+
+    # Ruby 2.6.X does not support comparing Gem::Version object with string.
+    #   Gem::Version.new(x) >= '0.3.3' #=> Exception
+    def initialize(str)
+      @version = Gem::Version.new(str)
+    end
+
+    def <=>(other)
+      @version <=> Gem::Version.new(other)
     end
   end
 end
