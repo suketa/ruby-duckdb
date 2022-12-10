73b2c1019bee81abfd3b77203cad158e14bfe4c7
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-12-11 08:49:20 +0900

    implement DuckDB.library_version

--- ext/duckdb/duckdb.c
+++ ext/duckdb/duckdb.c
@@ -2,9 +2,23 @@
 
 VALUE mDuckDB;
 
+#ifdef HAVE_DUCKDB_H_GE_V060
+static VALUE duckdb_s_library_version(VALUE self);
+#endif
+
+#ifdef HAVE_DUCKDB_H_GE_V060
+static VALUE duckdb_s_library_version(VALUE self) {
+  return rb_str_new2(duckdb_library_version());
+}
+#endif
+
 void
 Init_duckdb_native(void) {
     mDuckDB = rb_define_module("DuckDB");
+
+#ifdef HAVE_DUCKDB_H_GE_V060
+    rb_define_singleton_method(mDuckDB, "library_version", duckdb_s_library_version, 0);
+#endif
 
     init_duckdb_error();
     init_duckdb_database();
