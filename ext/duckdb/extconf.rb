21b3479a54d97a5ef209590670fd6aff2477c15c
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-12-10 09:19:24 +0900

    use duckdb_value_string instead of duckdb_value_varchar(#406).
    duckdb_value_varchar will be deprecated.

--- ext/duckdb/extconf.rb
+++ ext/duckdb/extconf.rb
@@ -10,6 +10,9 @@ raise 'duckdb >= 0.2.9 is required. Install duckdb >= 0.2.9' unless have_func('d
 # ducdb >= 0.3.3 if duckdb_append_data_chunk() is defined.
 have_func('duckdb_append_data_chunk', 'duckdb.h')
 
+# check duckdb >= 0.6.0
+have_func('duckdb_value_string', 'duckdb.h')
+
 have_func('duckdb_free', 'duckdb.h')
 
 create_makefile('duckdb/duckdb_native')
