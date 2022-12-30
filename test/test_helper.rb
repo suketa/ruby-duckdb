7ceccc9746a1cf5d3519d85b5cd02c079aecf53e
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-12-30 09:25:15 +0900

    fix GC.verify_compaction_references warning
    refs #416

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,11 @@ require 'duckdb'
 require_relative './duckdb_test/duckdb_version'
 
 if defined?(GC.verify_compaction_references) == 'method'
-  GC.verify_compaction_references(double_heap: true, toward: :empty)
+  if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.2.0')
+    GC.verify_compaction_references(expand_heap: true, toward: :empty)
+  else
+    GC.verify_compaction_references(double_heap: true, toward: :empty)
+  end
 end
 
 require 'minitest/autorun'
