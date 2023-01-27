9482510c2c8279d03bc9a41f56d65247acf55ba2
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2023-01-27 14:49:45 +0900

    refactor for checking DuckDB.library_version

--- test/duckdb_test.rb
+++ test/duckdb_test.rb
@@ -7,7 +7,7 @@ module DuckDBTest
     end
 
     def test_that_it_has_a_library_version_number
-      refute_nil ::DuckDB::LIBRARY_VERSION if DuckDB.methods.include?(:library_version)
+      refute_nil ::DuckDB::LIBRARY_VERSION if defined? DuckDB.library_version
     end
   end
 end
