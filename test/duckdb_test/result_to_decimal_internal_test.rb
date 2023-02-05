703c93b9fe7d6a92b31304fab395c28bbec37bbf
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2023-02-05 12:55:24 +0900

    change decimal width

--- test/duckdb_test/result_to_decimal_internal_test.rb
+++ test/duckdb_test/result_to_decimal_internal_test.rb
@@ -6,7 +6,7 @@ module DuckDBTest
     def setup
       @db = DuckDB::Database.open
       @con = @db.connect
-      @con.query('CREATE TABLE decimals (decimal_value DECIMAL(30,8))')
+      @con.query('CREATE TABLE decimals (decimal_value DECIMAL(38,8))')
     end
 
     def prepare_test_value(value)
@@ -25,35 +25,35 @@ module DuckDBTest
     end
 
     def test_result_to_decimal_positive1
-      do_result_to_decimal_test([123_400_000, 0, 30, 8], '1.234')
+      do_result_to_decimal_test([123_400_000, 0, 38, 8], '1.234')
     end
 
     def test_result_to_decimal_positive2
-      do_result_to_decimal_test([123_456_789, 0, 30, 8], '1.23456789')
+      do_result_to_decimal_test([123_456_789, 0, 38, 8], '1.23456789')
     end
 
     def test_result_to_decimal_positive3
-      do_result_to_decimal_test([123_456_789, 0, 30, 8], '1.234567898')
+      do_result_to_decimal_test([123_456_789, 0, 38, 8], '1.234567898')
     end
 
     def test_result_to_decimal_positive4
-      do_result_to_decimal_test([123_456, 0, 30, 8], '0.00123456789')
+      do_result_to_decimal_test([123_456, 0, 38, 8], '0.00123456789')
     end
 
     def test_result_to_decimal_positive5
-      do_result_to_decimal_test([123_456, 0, 30, 8], '0.00123456')
+      do_result_to_decimal_test([123_456, 0, 38, 8], '0.00123456')
     end
 
     def test_result_to_decimal_positive6
-      do_result_to_decimal_test([-123_456, -1, 30, 8], '-0.00123456')
+      do_result_to_decimal_test([-123_456, -1, 38, 8], '-0.00123456')
     end
 
     def test_result_to_decimal_positive7
-      do_result_to_decimal_test([6_634_324_952_100_531_253, 12_715_950_803, 30, 8], '2345678901234567890123.45678901')
+      do_result_to_decimal_test([6_634_324_952_100_531_253, 12_715_950_803, 38, 8], '2345678901234567890123.45678901')
     end
 
     def test_result_to_decimal_positive8
-      do_result_to_decimal_test([-6_634_324_952_100_531_253, -12_715_950_804, 30, 8], '-2345678901234567890123.45678901')
+      do_result_to_decimal_test([-6_634_324_952_100_531_253, -12_715_950_804, 38, 8], '-2345678901234567890123.45678901')
     end
   end
 end
