4725f50ed7eea371a8303f8fe093cc871f185b6e
suketa <masaki.suketa@nifty.ne.jp>
2023-02-07 21:36:18 +0900

    add Decimal#__to_decimal_internal (#452)
    * add Decimal#__to_decimal_internal
    
    * add test for bigdecimal
    
    * add Result#__to_decimal_internal test
    
    * change decimal width
    
    * fix converting:
    decimal to ruby [lower, upper, width, scale]
    
    * fix conversion hugeint to ruby [lower, upper]

--- test/duckdb_test/result_hugeint_internal_test.rb
+++ test/duckdb_test/result_hugeint_internal_test.rb
@@ -17,51 +17,51 @@ module DuckDBTest
       @db.close
     end
 
-    def do_result_hugeint_internal_test(value, lower, upper)
+    def do_result_hugeint_internal_test(expected, value)
       prepare_test_value(value)
       result = @con.query('SELECT hugeint_value FROM hugeints')
-      assert_equal([lower, upper], result.send(:__to_hugeint_internal, 0, 0))
+      assert_equal(expected, result.send(:__to_hugeint_internal, 0, 0))
     end
 
     def test_result_internal_positive1
-      do_result_hugeint_internal_test(1, 1, 0)
+      do_result_hugeint_internal_test([1, 0], 1)
     end
 
     def test_hugeint_internal_zero
-      do_result_hugeint_internal_test(0, 0, 0)
+      do_result_hugeint_internal_test([0, 0], 0)
     end
 
     def test_hugeint_internal_negative1
-      do_result_hugeint_internal_test(-1, -1, -1)
+      do_result_hugeint_internal_test([18446744073709551615, -1], -1)
     end
 
     def test_hugeint_internal_positive100
-      do_result_hugeint_internal_test(100, 100, 0)
+      do_result_hugeint_internal_test([100, 0], 100)
     end
 
     def test_hugeint_internal_val_negative100
-      do_result_hugeint_internal_test(-100, -100, -1)
+      do_result_hugeint_internal_test([18446744073709551516, -1], -100)
     end
 
     def test_hugeint_internal_negative_half_hugeint
-      do_result_hugeint_internal_test(-9_223_372_036_854_775_808, -9_223_372_036_854_775_808, -1)
+      do_result_hugeint_internal_test([9_223_372_036_854_775_808, -1], -9_223_372_036_854_775_808)
     end
 
     def test_hugeint_internal_negative_half_hugeint_minus_one
-      do_result_hugeint_internal_test(-9_223_372_036_854_775_809, 9_223_372_036_854_775_807, -1)
+      do_result_hugeint_internal_test([9_223_372_036_854_775_807, -1], -9_223_372_036_854_775_809)
     end
 
     def test_hugeint_internal_val_max
       do_result_hugeint_internal_test(
-        170_141_183_460_469_231_731_687_303_715_884_105_727,
-        -1, 9_223_372_036_854_775_807
+        [18_446_744_073_709_551_615, 9_223_372_036_854_775_807],
+        170_141_183_460_469_231_731_687_303_715_884_105_727
       )
     end
 
     def test_hugeint_internal_val_min
       do_result_hugeint_internal_test(
-        -170_141_183_460_469_231_731_687_303_715_884_105_727,
-        1, -9_223_372_036_854_775_808
+        [1, -9_223_372_036_854_775_808],
+        -170_141_183_460_469_231_731_687_303_715_884_105_727
       )
     end
   end
