1bfe86c01372224bc226fa9e76c36412a1557942
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2023-02-10 14:29:48 +0900

    Revert "Replace DuckDB::LIBRARY_VERSION"
    This reverts commit 43b84f56f6970a559f1418d2679d3f8200dc82e8.

--- lib/duckdb/version.rb
+++ lib/duckdb/version.rb
@@ -2,5 +2,4 @@ module DuckDB
   # The version string of ruby-duckdb.
   # Currently, ruby-duckdb is NOT semantic versioning.
   VERSION = '0.6.1'.freeze
-  LIBRARY_VERSION = library_version[1..] if defined? library_version
 end
