65d3b9fe4b0084d37c8442d78e3904b6c4f7035b
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2023-04-15 06:19:30 +0900

    add NG test for PreparedStatement#bind Decimal value

---
+++ test/duckdb_test/prepare_statement_decimal_test.rb
@@ -0,0 +1,26 @@
+module DuckDBTest
+  class PreparedStatementDecimalTest < Minitest::Test
+    def setup
+      @db = DuckDB::Database.open
+      @con = @db.connect
+      @con.query('CREATE TABLE decimals (decimal_value HUGEINT)')
+    end
+
+    def prepare_test_value(value)
+      @con.query("INSERT INTO decimals VALUES (#{value})")
+      @prepared = @con.prepared_statement('SELECT * FROM decimals WHERE decimal_value = ?')
+    end
+
+    def teardown
+      @con.close
+      @db.close
+    end
+
+    # FIXME: @prepared.bind(1, BigDecimal('1.0')) should not raise DuckDB::Error.
+    def NG_test_decimal
+      prepare_test_value(1.0)
+      r = @prepared.bind(1, BigDecimal('1.0')).execute
+      assert_equal(1.0, r.first.first)
+    end
+  end
+end
