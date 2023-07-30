6ffff5b7c0adcd4be6bb169f833ab97cc785acc8
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2023-07-30 14:04:36 +0900

    add DuckDB::Inteval.mk_interval test.

--- test/duckdb_test/interval_test.rb
+++ test/duckdb_test/interval_test.rb
@@ -42,6 +42,45 @@ module DuckDBTest
       assert_raises(ArgumentError) { DuckDB::Interval.iso8601_parse('1Y2M3DT4H5M6.7') }
     end
 
+    def test_s_mk_interval
+      assert_equal(
+        DuckDB::Interval.new(interval_months: 14, interval_days: 3, interval_micros: 14_706_700_000),
+        DuckDB::Interval.mk_interval(year: 1, month: 2, day: 3, hour: 4, min: 5, sec: 6, usec: 700_000)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_months: 14, interval_days: 3, interval_micros: 14_706_000_007),
+        DuckDB::Interval.mk_interval(year: 1, month: 2, day: 3, hour: 4, min: 5, sec: 6, usec: 7)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_months: 12),
+        DuckDB::Interval.mk_interval(year: 1)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_months: 1),
+        DuckDB::Interval.mk_interval(month: 1)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_days: 1),
+        DuckDB::Interval.mk_interval(day: 1)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_micros: 3_600_000_000),
+        DuckDB::Interval.mk_interval(hour: 1)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_micros: 60_000_000),
+        DuckDB::Interval.mk_interval(min: 1)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_micros: 1_000_000),
+        DuckDB::Interval.mk_interval(sec: 1)
+      )
+      assert_equal(
+        DuckDB::Interval.new(interval_micros: 1),
+        DuckDB::Interval.mk_interval(usec: 1)
+      )
+    end
+
     def test_initialize
       assert_instance_of(DuckDB::Interval, DuckDB::Interval.new)
     end
