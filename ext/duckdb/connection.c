4c4336e40fc51e7ca8bba7c7110b1473f66e06e6
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2023-11-04 20:43:07 +0900

    add DuckDB::Connection#interrupt, DuckDB::Connection#query_progress.

--- ext/duckdb/connection.c
+++ ext/duckdb/connection.c
@@ -6,6 +6,12 @@ static void deallocate(void *ctx);
 static VALUE allocate(VALUE klass);
 static size_t memsize(const void *p);
 static VALUE duckdb_connection_disconnect(VALUE self);
+
+#ifdef HAVE_DUCKDB_H_GE_V090
+static VALUE duckdb_connection_interrupt(VALUE self);
+static VALUE duckdb_connection_query_progress(VALUE self);
+#endif
+
 static VALUE duckdb_connection_connect(VALUE self, VALUE oDuckDBDatabase);
 static VALUE duckdb_connection_query_sql(VALUE self, VALUE str);
 
@@ -63,6 +69,26 @@ static VALUE duckdb_connection_disconnect(VALUE self) {
 
     return self;
 }
+#ifdef HAVE_DUCKDB_H_GE_V090
+static VALUE duckdb_connection_interrupt(VALUE self) {
+    rubyDuckDBConnection *ctx;
+
+    TypedData_Get_Struct(self, rubyDuckDBConnection, &connection_data_type, ctx);
+    duckdb_interrupt(ctx->con);
+
+    return self;
+}
+
+static VALUE duckdb_connection_query_progress(VALUE self) {
+    rubyDuckDBConnection *ctx;
+    double progress;
+
+    TypedData_Get_Struct(self, rubyDuckDBConnection, &connection_data_type, ctx);
+    progress = duckdb_query_progress(ctx->con);
+
+    return DBL2NUM(progress);
+}
+#endif
 
 static VALUE duckdb_connection_connect(VALUE self, VALUE oDuckDBDatabase) {
     rubyDuckDBConnection *ctx;
@@ -105,6 +131,10 @@ void rbduckdb_init_duckdb_connection(void) {
     rb_define_alloc_func(cDuckDBConnection, allocate);
 
     rb_define_method(cDuckDBConnection, "disconnect", duckdb_connection_disconnect, 0);
+#ifdef HAVE_DUCKDB_H_GE_V090
+    rb_define_method(cDuckDBConnection, "interrupt", duckdb_connection_interrupt, 0);
+    rb_define_method(cDuckDBConnection, "query_progress", duckdb_connection_query_progress, 0);
+#endif
     rb_define_private_method(cDuckDBConnection, "_connect", duckdb_connection_connect, 1);
     rb_define_private_method(cDuckDBConnection, "query_sql", duckdb_connection_query_sql, 1);
 }
