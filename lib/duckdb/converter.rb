a0e514d2e5a804560410f438af1191a42ecb7e44
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2023-12-17 09:38:27 +0900

    refactoring

--- lib/duckdb/converter.rb
+++ lib/duckdb/converter.rb
@@ -23,10 +23,10 @@ module DuckDB
       Time.parse(
         format(
           '%<hour>02d:%<minute>02d:%<second>02d.%<microsecond>06d',
-          hour: hour,
-          minute: minute,
-          second: second,
-          microsecond: microsecond
+          hour:,
+          minute:,
+          second:,
+          microsecond:
         )
       )
     end
