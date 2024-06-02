b611a94e77f6c5e3887236a1d7a809774c4f227f
suketa <masaki.suketa@nifty.ne.jp>
2024-06-02 13:43:11 +0900

    implement DuckDB::ExtractedStatements (#636)
    * implement DuckDB::ExtractedStatements
    
    - raise error when `DuckDB::ExtractedStatements#new` is called with
      invalid SQL.
    - add `DuckDB::ExtractedStatements#size`.
    
    * fix error message

--- test/duckdb_test/extracted_statements_test.rb
+++ test/duckdb_test/extracted_statements_test.rb
@@ -20,6 +20,18 @@ module DuckDBTest
       end
     end
 
+    def test_s_new_with_invalid_sql
+      ex = assert_raises DuckDB::Error do
+        DuckDB::ExtractedStatements.new(@con, 'SELECT 1; INVALID STATEMENT; SELECT 3')
+      end
+      assert_match(/\AParser Error: syntax error at or near "INVALID"/, ex.message)
+    end
+
+    def test_size
+      stmts = DuckDB::ExtractedStatements.new(@con, 'SELECT 1; SELECT 2; SELECT 3')
+      assert_equal 3, stmts.size
+    end
+
     def teardown
       @con.close
       @db.close
