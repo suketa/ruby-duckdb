a0ae50736efcf4e92001b2cf8e9859f741151c9b
suketa <masaki.suketa@nifty.ne.jp>
2024-06-02 19:40:30 +0900

    add frozen_string_literal: true (#637)

--- lib/duckdb/library_version.rb
+++ lib/duckdb/library_version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module DuckDB
   LIBRARY_VERSION = library_version[1..]
 end
--- lib/duckdb/pending_result.rb
+++ lib/duckdb/pending_result.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module DuckDB
   # The DuckDB::PendingResult encapsulates connection with DuckDB pending
   # result.
