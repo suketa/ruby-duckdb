90ffe93e274d2db3da32cb92a58d8a63f6ede1c0
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2024-06-08 09:31:18 +0900

    prepare deprecated C-API functions.

--- ext/duckdb/pending_result.c
+++ ext/duckdb/pending_result.c
@@ -39,6 +39,13 @@ static VALUE duckdb_pending_result_initialize(int argc, VALUE *argv, VALUE self)
     VALUE streaming_p = Qfalse;
     duckdb_state state;
 
+    /*
+     * FIXME: The 2nd argument is deprecated and will be removed in the future.
+     * The behavior will be same as streaming.
+    if (argc == 2) {
+        rb_warn("The 2nd argument is deprecated and will be removed in the future.");
+    }
+    */
     rb_scan_args(argc, argv, "11", &oDuckDBPreparedStatement, &streaming_p);
 
     if (rb_obj_is_kind_of(oDuckDBPreparedStatement, cDuckDBPreparedStatement) != Qtrue) {
@@ -51,6 +58,11 @@ static VALUE duckdb_pending_result_initialize(int argc, VALUE *argv, VALUE self)
 #ifdef DUCKDB_API_NO_DEPRECATED
     state = duckdb_pending_prepared(stmt->prepared_statement, &(ctx->pending_result));
 #else
+    /*
+     * FIXME: streaming_p check will be removed in the future.
+     *
+     * state = duckdb_pending_prepared(stmt->prepared_statement, &(ctx->pending_result));
+     */
     if (!NIL_P(streaming_p) && streaming_p == Qtrue) {
         state = duckdb_pending_prepared_streaming(stmt->prepared_statement, &(ctx->pending_result));
     } else {
