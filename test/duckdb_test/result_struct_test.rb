e7f9227db61af41bc9a26d0331bfcd14e3ce4557
suketa <masaki.suketa@nifty.ne.jp>
2024-06-16 21:12:20 +0900

    support STRUCT column with DuckDB::Result (#664)
    * support STRUCT column with DuckDB::Result
    
    * change STRUCT key to symbol from string.
    
    * add DuckDB:Result STRUCT column test.
    
    * update CHANGELOG. STRUCT column support.

---
+++ test/duckdb_test/result_struct_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+module DuckDBTest
+  class ResultListTest < Minitest::Test
+    def setup
+      @db = DuckDB::Database.open
+      @conn = @db.connect
+    end
+
+    def test_result_struct
+      @conn.execute('CREATE TABLE test (s STRUCT(v VARCHAR, i INTEGER));')
+      @conn.execute("INSERT INTO test VALUES (ROW('abc', 12));")
+      @conn.execute("INSERT INTO test VALUES (ROW('de', 5));")
+      result = @conn.execute('SELECT * FROM test;')
+      ary = result.each.to_a
+      assert_equal({ v: 'abc', i: 12 }, ary.first[0])
+      assert_equal({ v: 'de', i: 5 }, ary.last[0])
+    end
+
+    def test_result_struct_with_nil
+      @conn.execute('CREATE TABLE test (s STRUCT(v VARCHAR, i INTEGER));')
+      @conn.execute("INSERT INTO test VALUES (ROW('abc', 12));")
+      @conn.execute('INSERT INTO test VALUES (ROW(NULL, 5));')
+      result = @conn.execute('SELECT * FROM test;')
+      ary = result.each.to_a
+      assert_equal({ v: 'abc', i: 12 }, ary.first[0])
+      assert_equal({ v: nil, i: 5 }, ary.last[0])
+    end
+
+    def test_result_struct_with_key_having_space
+      @conn.execute('CREATE TABLE test (s STRUCT("v 1" VARCHAR, i INTEGER));')
+      @conn.execute("INSERT INTO test VALUES (ROW('abc', 12));")
+      result = @conn.execute('SELECT * FROM test;')
+      ary = result.each.to_a
+      assert_equal({ 'v 1': 'abc', i: 12 }, ary.first[0])
+    end
+
+    def test_result_struct_struct
+      @conn.execute('CREATE TABLE test (s STRUCT(v STRUCT(a VARCHAR, b INTEGER), i INTEGER));')
+      @conn.execute("INSERT INTO test VALUES (ROW(ROW('abc', 12), 34));")
+      result = @conn.execute('SELECT * FROM test;')
+      ary = result.each.to_a
+      assert_equal({ v: { a: 'abc', b: 12 }, i: 34 }, ary.first[0])
+    end
+
+    def teardown
+      @conn.execute('DROP TABLE test;')
+      @conn.close
+      @db.close
+    end
+  end
+end
