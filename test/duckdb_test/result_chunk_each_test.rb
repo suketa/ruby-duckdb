c2ef1f4c80b814bc39588e768a3803a46831c85a
suketa <masaki.suketa@nifty.ne.jp>
2024-06-17 05:23:30 +0900

    fix STRUCT test in result_chunk_each_test (#666)

--- test/duckdb_test/result_chunk_each_test.rb
+++ test/duckdb_test/result_chunk_each_test.rb
@@ -72,9 +72,9 @@ module DuckDBTest
       [:ok, 'ARRAY',     'VARCHAR[2]',                  "array_value('a', '𝘶ñîҫȫ𝘥ẹ 𝖘ţ𝗋ⅰɲ𝓰 😃')",    Array,                ['a', '𝘶ñîҫȫ𝘥ẹ 𝖘ţ𝗋ⅰɲ𝓰 😃']                          ],
       [:ok, 'LIST',      'INTEGER[]',                   '[1, 2]',                                   Array,                [1, 2]                                              ],
       [:ok, 'LIST',      'INTEGER[][]',                 '[[1, 2], [3, 4]]',                         Array,                [[1, 2], [3, 4]]                                    ],
-      # FIXME: MAP STRUCT values are always nil
+      # FIXME: MAP values are always nil
       [:ng, 'MAP',       'MAP(INTEGER, INTEGER)',       'map {1: 2, 3: 4}',                         Hash,                 {1 => 2, 3 => 4}                                    ],
-      [:ng, 'STRUCT',    'STRUCT(a INTEGER, b INTEGER)', "{'a': 1, 'b': 2}",                        Hash,                 {"a" => 1, "b" => 2 }                               ],
+      [:ok, 'STRUCT',    'STRUCT(a INTEGER, b INTEGER)', "{'a': 1, 'b': 2}",                        Hash,                 {a: 1, b: 2 }                                       ],
     ].freeze
 
     def prepare_test_table_and_data(db_declaration, db_type, string_rep)
