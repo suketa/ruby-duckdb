d6866b7f0ce34c0dcaec8cdf8dcfc4e82fa7a59f
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2024-06-22 08:29:04 +0900

    add more bit test

--- test/duckdb_test/result_bit_test.rb
+++ test/duckdb_test/result_bit_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 module DuckDBTest
@@ -13,17 +15,32 @@ module DuckDBTest
       @conn.execute("INSERT INTO test VALUES ('0101101'::BIT);")
       @conn.execute("INSERT INTO test VALUES ('0'::BIT);")
       @conn.execute("INSERT INTO test VALUES ('00000000'::BIT);")
-      @conn.execute("INSERT INTO test VALUES ('1010101001'::BIT);")
+      result = @conn.execute('SELECT value FROM test;')
+      ary = result.each.to_a
+      assert_equal([['1'], ['0101101'], ['0'], ['00000000']], ary)
+    end
+
+    def test_result_bit_over_8_bits
+      @conn.execute('CREATE TABLE test (value BIT);')
+      over_8_bits = '1010101001'
+      @conn.execute("INSERT INTO test VALUES ('#{over_8_bits}'::BIT);")
+      result = @conn.execute('SELECT value FROM test;')
+      ary = result.each.to_a
+      assert_equal([[over_8_bits]], ary)
+    end
+
+    def test_result_bit_long
+      @conn.execute('CREATE TABLE test (value BIT);')
       long_bits = '11111111111111111111111111111111111110101010101010101010101010101010101010101011100000000'
       @conn.execute("INSERT INTO test VALUES ('#{long_bits}'::BIT);")
       result = @conn.execute('SELECT value FROM test;')
       ary = result.each.to_a
-      assert_equal([["1"], ["0101101"], ["0"], ["00000000"], ["1010101001"], [long_bits]], ary)
+      assert_equal([[long_bits]], ary)
     end
 
     def test_result_bit_nil
       @conn.execute('CREATE TABLE test (value BIT);')
-      @conn.execute("INSERT INTO test VALUES (NULL);")
+      @conn.execute('INSERT INTO test VALUES (NULL);')
       result = @conn.execute('SELECT value FROM test;')
       ary = result.each.to_a
       assert_equal([[nil]], ary)
@@ -36,4 +53,3 @@ module DuckDBTest
     end
   end
 end
-
