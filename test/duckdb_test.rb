9dc52245e655c237a506ed70a3449cdc8acc21d6
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2024-09-08 09:10:42 +0900

    refactoring. remove library version check.

--- test/duckdb_test.rb
+++ test/duckdb_test.rb
@@ -9,7 +9,6 @@ module DuckDBTest
     end
 
     def test_that_it_has_a_library_version_number
-      skip 'library_version is unavailable' unless defined? DuckDB.library_version
       refute_nil ::DuckDB::LIBRARY_VERSION
     end
   end
