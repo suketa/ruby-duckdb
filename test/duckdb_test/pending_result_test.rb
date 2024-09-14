f626b5de190ce38fcd57dbfbd1a64dd05fb2fb55
suketa <masaki.suketa@nifty.ne.jp>
2024-09-15 07:43:57 +0900

    try to fix flaky test. (#750)
    * try to fix flaky test.
    
    * try to fix CI error.

--- test/duckdb_test/pending_result_test.rb
+++ test/duckdb_test/pending_result_test.rb
@@ -7,54 +7,59 @@ module DuckDBTest
     def setup
       @db = DuckDB::Database.open
       @con = @db.connect
-      @con.query('CREATE TABLE int_vals (int_val INTEGER)')
-      @con.query('INSERT INTO int_vals VALUES (1), (2), (3), (4), (5)')
-      @stmt = @con.prepared_statement('SELECT * FROM int_vals WHERE int_val in (SELECT int_val FROM int_vals)')
+      # @con.query('CREATE TABLE int_vals (int_val INTEGER)')
+      # @con.query('INSERT INTO int_vals VALUES (1), (2), (3), (4), (5)')
+      @con.query('SET threads=1')
+      @con.query('CREATE TABLE tbl as SELECT range a, mod(range, 10) b FROM range(10000)')
+      @con.query('CREATE TABLE tbl2 as SELECT range a, FROM range(10000)')
+      @con.query('SET ENABLE_PROGRESS_BAR=true')
+      @con.query('SET ENABLE_PROGRESS_BAR_PRINT=false')
+      @stmt = @con.prepared_statement('SELECT count(*) FROM tbl where a = (SELECT min(a) FROM tbl2)')
+      # pending_result = @con.async_query('SELECT count(*) FROM tbl where a = (SELECT min(a) FROM tbl2)')
     end
 
     def test_state
       pending_result = @stmt.pending_prepared
       assert_equal :not_ready, pending_result.state
+
+      pending_result.execute_task while pending_result.state == :not_ready
+      assert_equal(:ready, pending_result.state)
+
+      pending_result.execute_pending
+      assert_equal(:ready, pending_result.state)
+
       pending_result.execute_task
-      sleep 0.01
-      pending_result.execute_task
-      assert_includes(%i[ready no_tasks], pending_result.state)
+      assert_equal(:error, pending_result.state)
     end
 
     def test_execution_finished?
       pending_result = @stmt.pending_prepared
       skip 'execution_finished? is not available' unless pending_result.respond_to? :execution_finished?
-
       assert_equal false, pending_result.execution_finished?
-      pending_result.execute_task
-      expected = pending_result.state == :ready
-      assert_equal expected, pending_result.execution_finished?
 
-      sleep 0.01
+      pending_result.execute_task while pending_result.state == :not_ready
+      assert_equal true, pending_result.execution_finished?
 
       pending_result.execute_task
-      expected = pending_result.state == :ready
-      assert_equal expected, pending_result.execution_finished?
+      assert_equal true, pending_result.execution_finished?
     end
 
     def test_execute_pending
       pending_result = @stmt.pending_prepared
-      while pending_result.state != :ready
-        sleep 0.01
-        pending_result.execute_task
-      end
+      pending_result.execute_task while pending_result.state == :not_ready
       assert_equal :ready, pending_result.state
-      assert_equal [[1], [2], [3], [4], [5]], pending_result.execute_pending.to_a
+      assert_equal [[1]], pending_result.execute_pending.to_a
     end
 
     def test_execute_check_state
-      pending_result = @stmt.pending_prepared
+      pending_result = @stmt.pending_prepared_stream
       state = pending_result.execute_check_state
-      assert_includes(%i[error no_tasks], state)
-      pending_result.execute_task
-      sleep 0.01
+      assert_equal(:no_tasks, state)
+
+      pending_result.execute_task while pending_result.state == :not_ready
+
       state = pending_result.execute_check_state
-      assert_includes(%i[no_tasks ready error], state)
+      assert_includes(%i[error ready], state)
     end
 
     def teardown
