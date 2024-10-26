ec8d0345e1c87435f00df553c867770e220344d7
otegami <a.s.takuya1026@gmail.com>
2024-10-26 09:17:28 +0800

    add DuckDB::PreparedStatement#_bind_decimal
    fix GH-#407
    
    Implement DuckDB::PreparedStatement#_bind_decimal using
    duckdb_bind_decimal.
    
    FYI: The following is the infromation of the function and type.
    
    ```cpp
    duckdb_state duckdb_bind_decimal(
      duckdb_prepared_statement prepared_statement,
      idx_t param_idx,
      duckdb_decimal val
    );
    
    typedef struct {
    	uint8_t width;
    	uint8_t scale;
    	duckdb_hugeint value;
    } duckdb_decimal;
    
    typedef struct {
    	uint64_t lower;
    	int64_t upper;
    } duckdb_hugeint;
    ```
    
    ref: https://duckdb.org/docs/api/c/api.html#duckdb_bind_decimal

--- ext/duckdb/prepared_statement.c
+++ ext/duckdb/prepared_statement.c
@@ -32,6 +32,7 @@ static VALUE duckdb_prepared_statement__bind_time(VALUE self, VALUE vidx, VALUE 
 static VALUE duckdb_prepared_statement__bind_timestamp(VALUE self, VALUE vidx, VALUE year, VALUE month, VALUE day, VALUE hour, VALUE min, VALUE sec, VALUE micros);
 static VALUE duckdb_prepared_statement__bind_interval(VALUE self, VALUE vidx, VALUE months, VALUE days, VALUE micros);
 static VALUE duckdb_prepared_statement__bind_hugeint(VALUE self, VALUE vidx, VALUE lower, VALUE upper);
+static VALUE duckdb_prepared_statement__bind_decimal(VALUE self, VALUE vidx, VALUE lower, VALUE upper, VALUE width, VALUE scale);
 
 static const rb_data_type_t prepared_statement_data_type = {
     "DuckDB/PreparedStatement",
@@ -399,6 +400,26 @@ static VALUE duckdb_prepared_statement__bind_hugeint(VALUE self, VALUE vidx, VAL
     return self;
 }
 
+static VALUE duckdb_prepared_statement__bind_decimal(VALUE self, VALUE vidx, VALUE lower, VALUE upper, VALUE width, VALUE scale) {
+    duckdb_hugeint hugeint;
+    duckdb_decimal decimal;
+    rubyDuckDBPreparedStatement *ctx;
+    idx_t idx = check_index(vidx);
+
+    TypedData_Get_Struct(self, rubyDuckDBPreparedStatement, &prepared_statement_data_type, ctx);
+    hugeint.lower = NUM2ULL(lower);
+    hugeint.upper = NUM2LL(upper);
+    decimal.value = hugeint;
+    decimal.width = (uint8_t)NUM2UINT(width);
+    decimal.scale = (uint8_t)NUM2UINT(scale);
+
+    if (duckdb_bind_decimal(ctx->prepared_statement, idx, decimal) == DuckDBError) {
+        rb_raise(eDuckDBError, "fail to bind %llu parameter", (unsigned long long)idx);
+    }
+
+    return self;
+}
+
 rubyDuckDBPreparedStatement *get_struct_prepared_statement(VALUE self) {
     rubyDuckDBPreparedStatement *ctx;
     TypedData_Get_Struct(self, rubyDuckDBPreparedStatement, &prepared_statement_data_type, ctx);
@@ -434,4 +455,5 @@ void rbduckdb_init_duckdb_prepared_statement(void) {
     rb_define_private_method(cDuckDBPreparedStatement, "_bind_timestamp", duckdb_prepared_statement__bind_timestamp, 8);
     rb_define_private_method(cDuckDBPreparedStatement, "_bind_interval", duckdb_prepared_statement__bind_interval, 4);
     rb_define_private_method(cDuckDBPreparedStatement, "_bind_hugeint", duckdb_prepared_statement__bind_hugeint, 3);
+    rb_define_private_method(cDuckDBPreparedStatement, "_bind_decimal", duckdb_prepared_statement__bind_decimal, 5);
 }
