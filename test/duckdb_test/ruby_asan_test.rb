5e15a04a4219005be10b33470dc876517bbf7922
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 18:27:43 +0900

    raise DuckDB::Error

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -7,8 +7,7 @@ module DuckDBTest
     def test_with_ruby_asan
       db = DuckDB::Database.open
       con = db.connect
-      con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
-      con.execute('INSERT INTO test VALUES (?, "hello")', 1)
+      # con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
       con.execute('INSERT INTO test VALUES (?, "hello")', 1)
       # stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello'); INSERT INTO test VALUES (1, 'world')")
       # stmts.each do |stmt|
