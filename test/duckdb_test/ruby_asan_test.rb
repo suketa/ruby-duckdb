e955ad1d21a5b2e99dfa4002821bd5460d77ccc4
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 18:15:44 +0900

    raise duplicate error

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -8,14 +8,10 @@ module DuckDBTest
       db = DuckDB::Database.open
       con = db.connect
       con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
-      con.execute("INSERT INTO test VALUES (1, 'hello'); INSERT INTO test VALUES (1, 'world')")
-      # # puts 'ExtractedStatements#each with PreapredStatement#(execute|destroy)'
-      # stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
-      # stmts.each do |stmt|
-      #   stmt.execute
-      #   stmt.destroy
-      # end
-      # # result = con.execute('SELECT * FROM test WHERE id = ?', 1)
+      stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello'); INSERT INTO test VALUES (1, 'world')")
+      stmts.each do |stmt|
+        stmt.execute
+      end
     end
   end
 end
