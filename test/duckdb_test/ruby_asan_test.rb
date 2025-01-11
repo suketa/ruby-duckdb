53fc7a093873ae0c20e8883855b365eea4774b1b
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 18:02:31 +0900

    fix duplicate key error

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -9,8 +9,8 @@ module DuckDBTest
       con = db.connect
       con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
       con.execute("INSERT INTO test VALUES (1, 'hello')")
-      # puts 'ExtractedStatements#each with PreapredStatement#(execute|destroy)'
-      stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
+      puts 'ExtractedStatements#each with PreapredStatement#(execute|destroy)'
+      stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (2, 'hello')")
       stmts.each do |stmt|
         stmt.execute
         stmt.destroy
