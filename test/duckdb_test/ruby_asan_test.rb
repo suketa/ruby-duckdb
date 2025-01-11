e77673026b879464324ba0940a10afe0d32a94b9
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-12 04:27:06 +0900

    more simple

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -4,7 +4,7 @@ require 'duckdb'
 def run_duckdb_asan_test
   db = DuckDB::Database.open
   con = db.connect
-  con.execute('INSERT INTO test VALUES (?, "hello")', 1)
+  stmt = DuckDB::PreparedStatement.new(con, 'INSERT INTO test VALUES (?, "hello")')
 rescue Exception => e
   p e
 end
