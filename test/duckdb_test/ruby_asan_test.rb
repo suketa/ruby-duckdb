880516a63df4a50b4132b3baeaea55511116aeba
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 16:55:01 +0900

    Revert "insert with prepared statement"
    This reverts commit dbe82f25e068398805ab938d5c42e291d7ce6609.
    
    ```
        def test_with_ruby_asan
          db = DuckDB::Database.open
          con = db.connect
          con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
          con.execute("INSERT INTO test VALUES (1, 'hello')")
          puts 'ExtractedStatements#each with PreapredStatement#(execute|destroy)'
          stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
          stmts.each do |stmt|
            stmt.execute
            stmt.destroy
          end
          result = con.execute('SELECT * FROM test WHERE id = ?', 1)
        end
    ```

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -5,17 +5,17 @@ require 'test_helper'
 module DuckDBTest
   class RubyAsanTest < Minitest::Test
     def test_with_ruby_asan
-      puts 'insert with prepared statement'
       db = DuckDB::Database.open
       con = db.connect
       con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
-      con.execute("INSERT INTO test VALUES (?, 'hello')", 1)
-      # stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
-      # stmts.each do |stmt|
-      #   stmt.execute
-      #   stmt.destroy
-      # end
-      # result = con.execute('SELECT * FROM test WHERE id = ?', 1)
+      con.execute("INSERT INTO test VALUES (1, 'hello')")
+      puts 'ExtractedStatements#each with PreapredStatement#(execute|destroy)'
+      stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
+      stmts.each do |stmt|
+        stmt.execute
+        stmt.destroy
+      end
+      result = con.execute('SELECT * FROM test WHERE id = ?', 1)
     end
   end
 end
