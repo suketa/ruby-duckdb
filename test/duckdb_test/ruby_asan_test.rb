53ba88423f1baf9d0c62567d9943de6995d5c069
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 18:33:32 +0900

    assert_raises

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -7,12 +7,7 @@ module DuckDBTest
     def test_with_ruby_asan
       db = DuckDB::Database.open
       con = db.connect
-      # con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
-      con.execute('INSERT INTO test VALUES (?, "hello")', 1)
-      # stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello'); INSERT INTO test VALUES (1, 'world')")
-      # stmts.each do |stmt|
-      #   stmt.execute
-      # end
+      assert_raises(DuckDB::Error) { con.execute('INSERT INTO test VALUES (?, "hello")', 1) }
     end
   end
 end
