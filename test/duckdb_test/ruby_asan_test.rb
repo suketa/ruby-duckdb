b59f2c185d50b5085bf197fd5f0a5aa48a695c54
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 15:44:25 +0900

    comment SELECT

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -9,7 +9,7 @@ module DuckDBTest
       con = db.connect
       con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
       con.execute('INSERT INTO test VALUES (1, "hello")')
-      result = con.execute('SELECT * FROM test WHERE id = ?', 1)
+      # result = con.execute('SELECT * FROM test WHERE id = ?', 1)
     end
   end
 end
