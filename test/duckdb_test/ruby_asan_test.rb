f5f026a4a947f7bf465ff9ab4df5707e7c3bab06
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 16:51:18 +0900

    insert and execute
    ```
        def test_with_ruby_asan
          puts 'insert with prepared statement and select'
          db = DuckDB::Database.open
          con = db.connect
          con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
          stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
          stmts.each do |stmt|
            stmt.execute
            stmt.destroy
          end
          result = con.execute('SELECT * FROM test WHERE id = ?', 1)
        end
    ```

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -5,17 +5,16 @@ require 'test_helper'
 module DuckDBTest
   class RubyAsanTest < Minitest::Test
     def test_with_ruby_asan
-      puts 'insert with prepared statement'
+      puts 'insert with prepared statement and select'
       db = DuckDB::Database.open
       con = db.connect
       con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
-      con.execute("INSERT INTO test VALUES (?, 'hello')", 1)
-      # stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
-      # stmts.each do |stmt|
-      #   stmt.execute
-      #   stmt.destroy
-      # end
-      # result = con.execute('SELECT * FROM test WHERE id = ?', 1)
+      stmts = DuckDB::ExtractedStatements.new(con, "INSERT INTO test VALUES (1, 'hello')")
+      stmts.each do |stmt|
+        stmt.execute
+        stmt.destroy
+      end
+      result = con.execute('SELECT * FROM test WHERE id = ?', 1)
     end
   end
 end
