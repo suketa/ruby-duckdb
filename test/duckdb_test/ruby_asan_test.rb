7ebaf6aa2c4ddf98f6e324202d689a25e63d04ab
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-01-11 15:34:17 +0900

    add connection#execute

--- test/duckdb_test/ruby_asan_test.rb
+++ test/duckdb_test/ruby_asan_test.rb
@@ -7,6 +7,7 @@ module DuckDBTest
     def test_with_ruby_asan
       db = DuckDB::Database.open
       con = db.connect
+      con.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100))')
     end
   end
 end
