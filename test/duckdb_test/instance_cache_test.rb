e7d262580edd88a91ec43ed3229ffa7fd6b0c916
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2025-04-12 23:39:13 +0900

    add test for get_or_create

--- test/duckdb_test/instance_cache_test.rb
+++ test/duckdb_test/instance_cache_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'fileutils'
 
 if defined?(DuckDB::InstanceCache)
 
@@ -8,6 +9,45 @@ module DuckDBTest
   class InstanceCacheTest < Minitest::Test
     def test_s_new
       assert_instance_of DuckDB::InstanceCache, DuckDB::InstanceCache.new
+    end
+
+    def test_get_or_create
+      cache = DuckDB::InstanceCache.new
+      path = 'test_shared_db.db'
+      30.times do
+        thread = Thread.new do
+          db = cache.get_or_create(path)
+          assert_instance_of DuckDB::Database, db
+          db.close
+        end
+        db = cache.get_or_create(path)
+        assert_instance_of DuckDB::Database, db
+        db.close
+        thread.join
+
+        FileUtils.rm_f(path)
+      end
+    end
+
+    def test_get_or_create_without_path
+      cache = DuckDB::InstanceCache.new
+      db = cache.get_or_create
+      assert_instance_of DuckDB::Database, db
+      db.close
+    end
+
+    def test_get_or_create_with_empty_path
+      cache = DuckDB::InstanceCache.new
+      db = cache.get_or_create('')
+      assert_instance_of DuckDB::Database, db
+      db.close
+    end
+
+    def test_get_or_create_with_memory
+      cache = DuckDB::InstanceCache.new
+      db = cache.get_or_create(':memory:')
+      assert_instance_of DuckDB::Database, db
+      db.close
     end
 
     def test_destroy
