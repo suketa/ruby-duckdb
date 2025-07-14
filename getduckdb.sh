8a6cf62feb423ad313f03848d1e023564b46c084
Victor Hiairrassary <victor.hiairrassary@gmail.com>
2025-07-14 12:22:03 +0100

    Fix getduckdb.sh with DuckDB >= 1.3.0

--- getduckdb.sh
+++ getduckdb.sh
@@ -3,8 +3,16 @@
 MACHINE=`uname -m`
 
 case "$MACHINE" in
-  "x86_64" ) ARC=amd64 ;;
-  "aarch64" ) ARC=aarch64 ;;
+  "x86_64")
+    ARCH=amd64
+    ;;
+  "aarch64")
+    if printf '%s\n' '1.3.0' "$DUCKDB_VERSION" | sort -C -V; then
+      ARCH=arm64
+    else
+      ARCH=aarch64
+    fi
+    ;;
 esac
 
-wget -O duckdb.zip "https://github.com/duckdb/duckdb/releases/download/v$DUCKDB_VERSION/libduckdb-linux-$ARC.zip"
+wget -O duckdb.zip "https://github.com/duckdb/duckdb/releases/download/v$DUCKDB_VERSION/libduckdb-linux-$ARCH.zip"
