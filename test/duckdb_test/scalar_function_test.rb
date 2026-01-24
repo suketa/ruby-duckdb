a6aa8066a74d3a016154655d8f40bab606f1f075
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2026-01-24 21:45:50 +0900

    Fix rubocop offenses in test/duckdb_test/scalar_function_test.rb
    Refactor test methods to use setup/teardown for database and connection
    management instead of inline initialization and cleanup. This reduces
    method length and eliminates Metrics/MethodLength violations.
    
    Changes:
    - Add setup method to open database and create connection
    - Add teardown method to close connection and database
    - Use @db and @con instance variables throughout tests
    - Remove ensure blocks from test methods
    - Reduce test_register_scalar_function from 13 to 10 lines
    - Reduce test_register_scalar_function_raises_error_without_single_thread
      from 14 to 10 lines

--- test/duckdb_test/scalar_function_test.rb
+++ test/duckdb_test/scalar_function_test.rb
@@ -4,6 +4,16 @@ require 'test_helper'
 
 module DuckDBTest
   class ScalarFunctionTest < Minitest::Test
+    def setup
+      @db = DuckDB::Database.open
+      @con = @db.connect
+    end
+
+    def teardown
+      @con&.close
+      @db&.close
+    end
+
     def test_initialize
       sf = DuckDB::ScalarFunction.new
 
@@ -45,31 +55,22 @@ module DuckDBTest
     end
 
     def test_register_scalar_function
-      db = DuckDB::Database.open
-      con = db.connect
-
       # Scalar functions with Ruby callbacks require single-threaded execution
-      con.execute('SET threads=1')
+      @con.execute('SET threads=1')
 
       sf = DuckDB::ScalarFunction.new
       sf.name = 'foo'
       sf.return_type = DuckDB::LogicalType.new(4) # INTEGER
       sf.set_function { 1 }
 
-      con.register_scalar_function(sf)
+      @con.register_scalar_function(sf)
 
-      result = con.execute('SELECT foo()')
+      result = @con.execute('SELECT foo()')
 
       assert_equal 1, result.first.first
-    ensure
-      con&.close
-      db&.close
     end
 
     def test_register_scalar_function_raises_error_without_single_thread
-      db = DuckDB::Database.open
-      con = db.connect
-
       sf = DuckDB::ScalarFunction.new
       sf.name = 'will_fail'
       sf.return_type = DuckDB::LogicalType.new(4) # INTEGER
@@ -77,14 +78,11 @@ module DuckDBTest
 
       # Should raise error because threads is not 1
       error = assert_raises(DuckDB::Error) do
-        con.register_scalar_function(sf)
+        @con.register_scalar_function(sf)
       end
 
       assert_match(/single-threaded execution/, error.message)
       assert_match(/SET threads=1/, error.message)
-    ensure
-      con&.close
-      db&.close
     end
   end
 end
