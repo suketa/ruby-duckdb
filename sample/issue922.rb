cee0d31dc747f42954d632a4ab37e6fb1b5cde5b
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2026-02-28 08:51:48 +0900

    refactoring

--- sample/issue922.rb
+++ sample/issue922.rb
@@ -11,36 +11,65 @@ df = Polars::DataFrame.new(
 )
 
 module DuckDB
-  class Connection
-    def register_as_table(name, df) # rubocop:disable Metrics/MethodLength, Naming/MethodParameterName
-      columns = df.columns.each_with_object({}) { |header, hash| hash[header] = LogicalType::VARCHAR }
-      counter = 0
-      height = df.height
-      width = df.columns.length
-      tf = DuckDB::TableFunction.create(
-        name:,
-        columns:
-      ) do |_func_info, output|
-        if counter < height
-          width.times do |index|
-            output.set_value(index, 0, df[counter, index])
-          end
-          counter += 1
-          1
-        else
-          counter = 0
-        end
+  class TableFunction
+    @table_adapters = {}
+    class << self
+      def add_table_adapter(klass, adapter)
+        @table_adapters[klass] = adapter
       end
+
+      def table_adapter_for(klass)
+        @table_adapters[klass]
+      end
+    end
+  end
+
+  class Connection
+    def create_table_function(object, name)
+      adapter = TableFunction.table_adapter_for(object.class)
+      raise ArgumentError, "No table adapter registered for #{object.class}" if adapter.nil?
+
+      tf = adapter.call(object, name)
       register_table_function(tf)
     end
   end
+
+  module Polars
+    module DataFrame
+      class TableAdapter
+        def call(df, name)
+          columns = df.columns.each_with_object({}) { |header, hash| hash[header] = LogicalType::VARCHAR }
+          counter = 0
+          height = df.height
+          width = df.columns.length
+
+          DuckDB::TableFunction.create(
+            name:,
+            columns:
+          ) do |_func_info, output|
+            if counter < height
+              width.times do |index|
+                output.set_value(index, 0, df[counter, index])
+              end
+              counter += 1
+              1
+            else
+              counter = 0
+              0
+            end
+          end
+        end
+      end
+    end
+  end
+  TableFunction.add_table_adapter(::Polars::DataFrame, DuckDB::Polars::DataFrame::TableAdapter.new)
 end
 
 db = DuckDB::Database.open
 
 con = db.connect
 con.query('SET threads=1')
-con.register_as_table('polars_df', df)
+con.create_table_function(df, 'polars_df')
 result = con.query('SELECT * FROM polars_df()').to_a
 p result
 puts result.first.first == '1'
