b93a5a5447a812c28f764b37eeccf3c3c2f1be7f
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2026-04-15 20:14:56 +0900

    refactoring. simplify Gc.verify_compaction_references
    We can write Ruby >= 3.2.0 style now.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,13 +5,7 @@ require 'duckdb'
 
 require_relative 'duckdb_test/duckdb_version'
 
-if defined?(GC.verify_compaction_references) == 'method'
-  if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.2.0')
-    GC.verify_compaction_references(expand_heap: true, toward: :empty)
-  else
-    GC.verify_compaction_references(double_heap: true, toward: :empty) unless /3.0/ =~ RUBY_VERSION
-  end
-end
+GC.verify_compaction_references(expand_heap: true, toward: :empty)
 
 module DuckDBTest
   def duckdb_library_version
